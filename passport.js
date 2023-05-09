83ba84df5fc0484b135256f1260e1766b83fbca4
faiz khan <kfaiz2215@gmail.com>
2023-05-09 12:57:33 +0530

    server

--- passport.js
+++ passport.js
@@ -1,4 +1,4 @@
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
 const passport = require("passport");
 
 passport.use(
