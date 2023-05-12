a5518f69b785328dd4b7ef43b6d539cecbf3f6b2
faiz khan <kfaiz2215@gmail.com>
2023-05-12 10:49:07 +0530

    urlencoded edded in client url

--- routes/auth.js
+++ routes/auth.js
@@ -7,11 +7,11 @@ const { generateJWT } = require('../middlewares/generateJWT');
 const jwt = require("jsonwebtoken");
 
 
-router.get("/login/success", (req, res) => {
-	// console.log("xhr", req.xhr );
+router.get("/api/user", (req, res) => {
+	console.log("user", req.user );
 	// console.log("isAuthenticated", req.isAuthenticated());
 
-	if (req.xhr && req.isAuthenticated()) {
+	if (req.user) {
 		// const token = jwt.sign({ user: req.user }, process.env.JWT_SECRET_DEV, { expiresIn: '1h' });
 		res.status(200).json({
 			error: false,
@@ -29,23 +29,31 @@ router.get("/login/failed", (req, res) => {
 		message: "Log in failure",
 	});
 });
-router.get("/google", passport.authenticate("google", ["openid", "profile", "email"]));
+// Create routes for authentication
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+// router.get("/google", passport.authenticate("google", ["openid", "profile", "email"]));
 // const clientUrl = process.env.CLIENT_URL || 'http://localhost:3000';
 
-router.get(
-	'/google/callback',
-	passport.authenticate('google', {
-		successRedirect: `${process.env.CLIENT_URL}`,
-		failureRedirect: '/login/failed',
-		// session: false,
-	}),
-	// (req, res) => {
-	// 	const token = generateJWT(req.user);
-	// 	console.log('token', token);
-	// 	res.cookie('token', token, { maxAge: 900000, httpOnly: true, secure: false, sameSite: 'none', domain: "https://quizzical-dltk.onrender.com", path: '/' });
-	// 	res.redirect("https://quizzical-dltk.onrender.com");
-	// },
-);
+// router.get(
+// 	'/google/callback',
+// 	passport.authenticate('google', {
+// 		successRedirect: `${process.env.CLIENT_URL}`,
+// 		failureRedirect: '/login/failed',
+// 		// session: false,
+// 	}),
+// 	// (req, res) => {
+// 	// 	const token = generateJWT(req.user);
+// 	// 	console.log('token', token);
+// 	// 	res.cookie('token', token, { maxAge: 900000, httpOnly: true, secure: false, sameSite: 'none', domain: "https://quizzical-dltk.onrender.com", path: '/' });
+// 	// 	res.redirect("https://quizzical-dltk.onrender.com");
+// 	// },
+// );
+
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+	// Redirect the user to the frontend with the user data in the query string
+	res.redirect(`${process.env.CLIENT_URL}/?user=${encodeURIComponent(JSON.stringify(req.user))}`);
+  });
 
 
 router.get('/logout', function (req, res) {
